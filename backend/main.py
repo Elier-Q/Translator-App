ae2dee472c2af2179d010d5401975f904a06c0dc
thealepo <sanchezc.alexe@gmail.com>
2025-03-29 18:47:46 -0400

    redo

--- backend/main.py
+++ backend/main.py
@@ -6,4 +6,19 @@ from typing import List
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(CORSMiddleware , allow_origins=origins , allow_credentials=True , allow_methods=["*"] , allow_headers=["*"])
+
+@app.get("/fruits" , response_model=Fruits)
+def get_fruits():
+    return Fruits(fruits=memory_db['fruits'])
+
+
+
+@app.get("/feed" , response_model=ExpoCamera)
+def get_feed():
+    return 
 
--- backend/service.py
+++ backend/service.py
@@ -1,4 +1,8 @@
 import cv2
+import cv2.text as cvt
 import pytesseract
 
-cap = cv2.VideoCapture()  #cap
+cap = cv2.VideoCapture()
+
+while True:
+    ret , frame = 
