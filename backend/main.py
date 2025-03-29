75cda96eca551c4898253fb70aae7a499b8be51f
Elier-Q <eliermq@gmail.com>
2025-03-29 14:51:53 -0400

    tesseract move info

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,13 @@
-from PIL import Image
-import pytesseract
+import pytesseract 
+from PIL import Image 
+import requests 
+import io
 
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
-print(pytesseract.image_to_string(Image.open('transapp/backend/Stop2.jpg')))
+
+##response = requests.get('https://pixabay.com/illustrations/stop-stop-sign-road-sign-7943805/') 
+##img = Image.open(io.BytesIO(response.content))
+img = 'transapp/backend/Stop2.jpg'
+text = pytesseract.image_to_string(Image.open(img), lang='eng', config='--psm 1')
+
+print(text)
