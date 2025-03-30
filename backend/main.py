220ec56377f99e2ef4aa2a18c2727c771349376e
thealepo <sanchezc.alexe@gmail.com>
2025-03-29 21:03:21 -0400

    backend skeleton

--- backend/main.py
+++ backend/main.py
@@ -7,18 +7,26 @@ from typing import List
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000"
+    "http://localhost:3000" #CHANGE THIS TO MATCH THE SERVER
 ]
 
-app.add_middleware(CORSMiddleware , allow_origins=origins , allow_credentials=True , allow_methods=["*"] , allow_headers=["*"])
+app.add_middleware(CORSMiddleware , 
+                   allow_origins=origins , 
+                   allow_credentials=True , 
+                   allow_methods=["*"] , 
+                   allow_headers=["*"])
 
-@app.get("/fruits" , response_model=Fruits)
-def get_fruits():
-    return Fruits(fruits=memory_db['fruits'])
 
+@app.get('/feed') #RETURN SOMETHING
+def get_feed():
+    return None 
 
 
 @app.get("/feed" , response_model=ExpoCamera)
 def get_feed():
-    return 
+    return None
+
+
+if __name__ == "__main__":
+    uvicorn.run(app , host="0.0.0.0" , port=8000)
 
--- tests/opencv.py
+++ tests/opencv.py
@@ -3,7 +3,7 @@ import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
 
-img = cv2.imread('transapp/tests/script.png')
+img = cv2.imread('transapp/tests/stop.png')
 
 gray = cv2.cvtColor(img , cv2.COLOR_BGR2GRAY)
 
