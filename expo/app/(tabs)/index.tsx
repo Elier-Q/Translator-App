4e81c993b6ba92948e9f819b9a70ef5909036ccd
Elier-Q <eliermq@gmail.com>
2025-03-29 21:02:25 -0400

    access to see live cam feed

--- expo/app/(tabs)/index.tsx
+++ expo/app/(tabs)/index.tsx
@@ -1,10 +1,40 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { Image, Button, Platform, StyleSheet,Text, View, Alert } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import {usePermissions} from 'expo-media-library'
+import { useCameraPermissions, useMicrophonePermissions } from 'expo-camera';
+import {router} from 'expo-router'
 
 export default function HomeScreen() {
+  const [cameraPermissions, requestCameraPermission] = useCameraPermissions();
+  const [microphonePermission, requestMicrophonePermission] = useMicrophonePermissions();
+  const [mediaLibraryPermissions, requestMediaLibraryPermission] = usePermissions();
+
+  async function handleContinue(){
+    const allPerms = await requestAllPerms();
+    if(allPerms){
+      router.replace("/(tabs)/explore")
+    }
+    else{
+      Alert.alert("To continue please provide permissions in settings")
+    }
+  }
+
+  async function requestAllPerms(){
+    const cameraStatus = await requestCameraPermission();
+    const micStatus = await requestMicrophonePermission();
+    //const medLibStatus = await requestMediaLibraryPermission();
+    if(!cameraStatus.granted || !micStatus.granted ){//|| !medLibStatus.granted
+      Alert.alert("Error", "All Permission Required");
+      return false;
+    }
+    await AsyncStorage.setItem("hasOpened", "true");
+    return true;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-    </View>
+    <><View style={styles.container}>
+      <Button title="Continue" onPress={handleContinue}/>
+    </View></>
+    
   );
 }
 
