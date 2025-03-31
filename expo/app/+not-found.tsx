b2402203ccd77da5f89f53942caa43a826472956
Elier-Q <eliermq@gmail.com>
2025-03-31 08:21:56 -0400

    Revised Sprint 3 Start

--- expo/app/+not-found.tsx
+++ expo/app/+not-found.tsx
@@ -1,7 +1,5 @@
 import { Link, Stack } from 'expo-router';
 import { StyleSheet } from 'react-native';
-import 'expo-dev-client';
-
 
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
--- expo/components/BottomRowTools.tsx
+++ expo/components/BottomRowTools.tsx
@@ -1,41 +1,60 @@
-import { StyleSheet, TouchableOpacity, View , Image} from "react-native";
-import IconButton from "./IconButtons";
+import * as React from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import IconButton from "./IconButton";
+import { Link } from "expo-router";
 import { ThemedText } from "./ThemedText";
-import { ThemedView } from "./ThemedView";
-import { CameraMode , Camera, CameraView} from "expo-camera";
-import 'expo-dev-client';
-import * as MediaLibrary from 'expo-media-library'
+import { CameraMode } from "expo-camera";
 
-export default function BottomRowTools(){
-    return(
-        <View style={[styles.bottomContainer,styles.directionRowItemsCenter]}>
-            <IconButton
-                iosName="photo.stack"
-                onPress = {() => {}}
-            />
-            <ThemedView>
-            <TouchableOpacity onPress={() => {
-                console.log("Happy bIRTHDAY")
-            }}> 
-                <ThemedText>Picture</ThemedText>
-            </TouchableOpacity>
-            </ThemedView>
-        </View>
-        
-    )
+interface BottomRowToolsProps {
+  cameraMode: CameraMode;
+  setCameraMode: React.Dispatch<React.SetStateAction<CameraMode>>;
+}
+export default function BottomRowTools({
+  cameraMode,
+  setCameraMode,
+}: BottomRowToolsProps) {
+  return (
+    <View style={[styles.bottomContainer, styles.directionRowItemsCenter]}>
+      <Link href={"/media-library"} asChild>
+        <IconButton androidName="library" iosName="photo.stack" />
+      </Link>
+      <View style={styles.directionRowItemsCenter}>
+        <TouchableOpacity onPress={() => setCameraMode("picture")}>
+          <ThemedText
+            style={{
+              fontWeight: cameraMode === "picture" ? "bold" : "100",
+            }}
+          >
+            Snap
+          </ThemedText>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setCameraMode("video")}>
+          <ThemedText
+            style={{
+              fontWeight: cameraMode === "video" ? "bold" : "100",
+            }}
+          >
+            Video
+          </ThemedText>
+        </TouchableOpacity>
+      </View>
+      <Link href={"/media-library"} asChild>
+        <IconButton androidName="add" iosName="magnifyingglass" />
+      </Link>
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-    directionRowItemsCenter: {
-        flexDirection: "row",
-        alignItems: "center",
-        gap: 12,
-    },
-    bottomContainer:{
-        width: "100%",
-        justifyContent: "center",
-        position: "absolute",
-        alignSelf: "center",
-        top: 200,
-    },
-
+  directionRowItemsCenter: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12,
+  },
+  bottomContainer: {
+    width: "100%",
+    justifyContent: "space-between",
+    position: "absolute",
+    alignSelf: "center",
+    bottom: 6,
+  },
 });
