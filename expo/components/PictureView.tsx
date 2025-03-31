f8a3bfeb925cd193844028e6d0c5e807b843c33c
Elier-Q <eliermq@gmail.com>
2025-03-31 10:52:26 -0400

    begin expo test 2

--- expo/components/PictureView.tsx
+++ expo/components/PictureView.tsx
@@ -37,18 +37,20 @@ export default function PictureView({ picture, setPicture }: PictureViewProps) {
             const imageBase64Uri = picture;  // Example Base64 string
 
             // FastAPI backend URL
-            const backendUrl = 'http://127.0.0.1:8000/image-uri';
+            const backendUrl = 'http://10.108.69.231:8000/image-uri';
 
             // Prepare the data to send (Base64 URI)
             //const data = { image_base64: imageBase64Uri };
 
             // Use Axios to send a POST request to FastAPI
-            axios.post(backendUrl, imageBase64Uri)
+            axios.post(backendUrl, {uri: imageBase64Uri})
               .then(response => {
                 console.log('Extracted text from image:', response.data.text);
+                Alert.alert(response.data.text);
               })
             .catch(error => {
                 console.error('Error:', error);
+                Alert.alert("error");
               });
             Alert.alert("✅ Picture saved!");
           }}
